40d48332083234ec73bdae65329b315369a583ce
Binod Adhikari <074msice006.binod@pcampus.edu.np>
2023-06-12 18:27:14 +0545

    pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,5 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-      region  = "us-east-1"
-    }
-  }
-}
-
-#Configure the AWS Provider
 provider "aws" {
+  version = "~> 4.0"
   region  = "us-east-1"
 }
 
